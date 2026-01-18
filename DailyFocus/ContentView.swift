f05e2411da856bb2a9dcf12e724d0a3b75b19791
Heli4m <ngohailam2010@gmail.com>
2026-01-18 21:22:01 +0700

    Initial Commit

---
+++ DailyFocus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DailyFocus
+//
+//  Created by Liam Ngo on 18/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DailyFocus/DailyFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyFocusApp.swift
+//  DailyFocus
+//
+//  Created by Liam Ngo on 18/1/26.
+//
+
+import SwiftUI
+
+@main
+struct DailyFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
